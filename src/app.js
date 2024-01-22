4389e8f852fb0705a4335be64c7064c286ae996b
Kas-999 <skashif977@gmail.com>
2024-01-23 01:49:49 +0530

    added new files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieparser from "cookie-parser"
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+dotenv.config();
 
 const app=express();
 
+if(process.env.NODE_ENV!=="production")
+{
+    app.use(morgan("dev"));
+}
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(mongoSanitize());
+app.use(cookieparser);
+app.use(compression());
+
+app.use(morgan("dev"));
+app.use(fileUpload(
+    {
+        useTempFiles:true,
+    }
+))
+app.use(
+    cors(
+        {
+            origin:"http://localhost:3000"
+        }
+    )
+)
 app.post('/test',(req,res)=> {
-    res.send("Hello from server");
+    res.send(req.body);
 })
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import app from "./app.js"
 
 
-dotenv.config();
 
 const PORT=process.env.PORT ||8000;
 
